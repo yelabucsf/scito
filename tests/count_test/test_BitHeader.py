da8f6d4f1ddbcdb172cc14f8316a5028beb7bad6
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2020-11-03 10:17:33 -0800

    v0.2.0 BitHeader constructor new test

--- tests/count_test/test_BitHeader.py
+++ tests/count_test/test_BitHeader.py
@@ -11,4 +11,5 @@ class TestAdtAtacBusHeader(TestCase):
         magic = struct.unpack('<4c', header[:4])
         version = struct.unpack('<L', header[4:8])
         bc_len = struct.unpack('<L', header[8:12])
+        tlen = struct.unpack('<L', header[16:20])
         self.assertEqual(bc_len[0], 21)
