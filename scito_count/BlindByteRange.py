593e63999cc1ac82f6cf419a152196bb209b9c46
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2020-11-28 09:51:38 -0800

    v0.2.0 feat (file_split): getting byte range of blind splits for SQS

--- scito_count/BlindByteRange.py
+++ scito_count/BlindByteRange.py
@@ -3,7 +3,7 @@ from scito_count.S3Interface import *
 from scito_count.ProcessSettings import *
 
 '''
-Class produces a blind catalog of large files producing a range of 5 Gb + 10 bytes (to avoid BGZF header split)
+Class produces a blind catalog of large files producing a range of 5 Gb + 18 bytes (to avoid BGZF header split)
 This is done because files > 12 Gb take over 1 min to get proper BlockCatalog, which is not suitable for lambda.
 Target - get files down to 5 Gb.
 '''
@@ -14,7 +14,11 @@ class BlindByteRange(object):
         window_start = 0
         while (window_start + int(5e9)) < obj_size:
             window_end = window_start + int(5e9)
-            window_start += window_end
+            window_start += window_end - 18 # 18 bytes is the BGZF full header
 
             # TODO send message to SQS with byte ranges
-            continue
+        if (window_start + int(5e9)) >= obj_size:
+            window_end = obj_size
+
+            # TODO send message to SQS with byte ranges
+
