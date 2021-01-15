7052ab3f01077c2e005f92f3d87f067e094d6433
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-01-15 11:36:03 -0800

    v0.2.0 feat (SQS interface) Made SQS read only operations. Tested

--- lambdas/file_splitter.py
+++
@@ -1,7 +0,0 @@
-from scito_count.S3BlockSplit import *
-
-# Trigger - with pattern "split"
-s3_set = S3Settings("/Users/antonogorodnikov/Documents/Work/Python/scito/tests/config_test.ini",
-                    "FULL R2 UPLOAD TEST")
-fileR1 = S3BlockSplit(s3_set)
-fileR1.generate_blocks()
