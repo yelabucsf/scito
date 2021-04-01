176a7f9604a8c749d9e76799783e29eeb76539ab
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-04-01 10:03:43 -0700

    v0.2.0 feat (lambda layers) - pushing layers

--- scripts/create_layers.sh
+++ scripts/create_layers.sh
@@ -5,10 +5,11 @@ CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 mkdir -p "${CURR_DIR}"/../layers
 
 
+# scito layer
+docker run  -v "${PWD}":/var/task "lambci/lambda:build-python3.8" /bin/bash -c "bash scito_layer.sh; exit"
 
-docker run \
-  -v "${PWD}":/var/task "lambci/lambda:build-python3.8" \
-  /bin/bash -c "bash scito_layer.sh; bash bustools_layer.sh; exit"
+# bustools layer
+#docker run  -v "${PWD}":/var/task "lambci/lambda:build-python3.8" /bin/bash -c "bash bustools_layer.sh; exit"
 
 mv *zip "${CURR_DIR}"/../layers/
 
